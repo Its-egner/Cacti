06c5bc5caca9a22345abd6afe8b5acf8186592aa
Its-egner <ronny_egner@web.de>
2025-06-11 07:42:13 +0200

    final edit

--- init-db.sh
+++ init-db.sh
@@ -1,7 +1,7 @@
 echo "Enter mysql Root Password for Setting up Database"
 read -p "Enter Root Password: " mysqlroot
 
-mysql -h $CACTI_DB_HOST -uroot -p$mysqlroot -e 'GRANT SELECT ON mysql.time_zone_name TO $CACTI_DB_NAME'
+mysql -h $CACTI_DB_HOST -uroot -p$mysqlroot -e "GRANT SELECT ON mysql.time_zone_name TO $CACTI_DB_USER"
 
 mysql -h $CACTI_DB_HOST -uroot -p$mysqlroot $CACTI_DB_NAME < /var/www/html/cacti/cacti.sql
 
